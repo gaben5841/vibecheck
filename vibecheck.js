ea723292afb159db210eb83395d4b46ad45d262d
Gaben <46634297+gaben5841@users.noreply.github.com>
2019-10-29 19:53:40 -0400

    the vibe checking begins

---
+++ vibecheck.js
@@ -0,0 +1,16 @@
+
+function checkVibe() {
+    var random_boolean = Math.random() >= 0.5;
+    console.log(random_boolean);
+    if (random_boolean) {
+        document.getElementById("type").innerHTML = "You have passed the vibe check";
+        document.getElementById("type").id = "boop";
+        
+    } else {
+        document.getElementById("type").innerHTML = "You have F A I L E D the vibe check";
+        document.getElementById("type").id = "boop";
+        document.getElementById("undeterminate").id = "failed";
+    }
+}
+
+setTimeout(function() { checkVibe(); } , 3000);
